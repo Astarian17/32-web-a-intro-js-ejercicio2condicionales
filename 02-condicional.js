1498e4d530caf842f25a2a5d9ae75fef9689733f
Mauricio Bracho <maubracho@gmail.com>
2024-02-17 22:02:46 -0700

    creacon segundo ejercicio condicionales

---
+++ 02-condicional.js
@@ -0,0 +1,8 @@
+const numero=Number(prompt("ingrese un numero"))
+
+console.log ("ingrese un numero")
+if (numero %2===0)
+{console.log("el numero es divisible entre 2")
+}
+else{ console.log("el numero no es divisible entre 2")
+}
